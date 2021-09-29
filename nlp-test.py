07d2c32fb96d82207a31cdacbd04e1e085b06c44
junyitang2-cyber <64450707+junyitang2-cyber@users.noreply.github.com>
2021-09-29 19:48:28 -0400

    Add files via upload

---
+++ nlp-test.py
@@ -0,0 +1,23 @@
+#This is TANG Junyi coding
+from google.cloud import language
+
+
+def analyze_text_sentiment(text):
+    client = language.LanguageServiceClient()
+    document = language.Document(content=text, type_=language.Document.Type.PLAIN_TEXT)
+
+    response = client.analyze_sentiment(document=document)
+
+    sentiment = response.document_sentiment
+    results = dict(
+        text=text,
+        score=f"{sentiment.score:.1%}",
+        magnitude=f"{sentiment.magnitude:.1%}",
+    )
+    for k, v in results.items():
+        print(f"{k:10}: {v}")
+
+if __name__ == "__main__":
+	text = "Guido van Rossum is great!"
+	analyze_text_sentiment(text)
+
